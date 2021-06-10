1cdf4bd6beb90ccbed9587969696001c3aa90184
attiq <marachckow2013@yandex.ru>
2021-06-10 21:57:18 +0300

    hiroku

--- target/ProjectUserTable-1.0-SNAPSHOT/WEB-INF/classes/dataBase.sql
+++ target/ProjectUserTable-1.0-SNAPSHOT/WEB-INF/classes/dataBase.sql
@@ -1,42 +1,33 @@
-
--- auto-generated definition
-create table m_user
+create table if not exists m_user
 (
-    id         bigserial   not null
+    id bigserial not null
         constraint m_user_pk
             primary key,
-    user_name  varchar(40) not null,
-    password   varchar(50) not null,
-    email      varchar(50) not null,
-    created    date        not null,
-    updated    date        not null,
+    user_name varchar(40) not null,
+    password varchar(50) not null,
+    email varchar(50) not null,
+    created date not null,
+    updated date not null,
     first_name varchar(50),
-    status     varchar(40)
+    status varchar(40)
 );
 
-alter table m_user
-    owner to postgres;
+alter table m_user owner to postgres;
 
-alter table m_user
-    owner to postgres;
-
--- auto-generated definition
-create table m_role
+create table if not exists m_role
 (
-    id        bigserial   not null
-        constraint m_role_pk
-            primary key,
+    id bigserial not null
+    constraint m_role_pk
+    primary key,
     role_name varchar(15) not null,
-    created   date        not null,
-    updated   date        not null,
-    status    varchar(25) not null
-);
+    created date not null,
+    updated date not null,
+    status varchar(25) not null
+    );
 
-alter table m_role
-    owner to postgres;
+alter table m_role owner to postgres;
 
--- auto-generated definition
-create table m_user_role
+create table if not exists m_user_role
 (
     id_role bigint not null
         constraint m_user_role_m_role__fk
@@ -46,5 +37,4 @@ create table m_user_role
             references m_user
 );
 
-alter table m_user_role
-    owner to postgres;
+alter table m_user_role owner to postgres;
